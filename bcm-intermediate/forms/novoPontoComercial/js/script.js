2b3ca862c1ec1d7074ec5c86405c2339a3ee5ff2
jorgeacruz <j.alexcruz@gmail.com>
2022-06-29 16:35:39 -0300

    Files and folder´s BPM/Intermediate Course

---
+++ bcm-intermediate/forms/novoPontoComercial/js/script.js
@@ -0,0 +1,14 @@
+
+var dataNascimento = FLUIGC.calendar('#MEUCALENDARIO');
+
+
+$("#cep").blur(function(){
+	
+    $.getJSON("//viacep.com.br/ws/" + $("#cep").val() +"/json/", function(dados){
+    $("#logradouro").val(dados.logradouro);
+    $("#bairro").val(dados.bairro);
+    $("#cidade").val(dados.localidade);
+    $("#estado").val(dados.uf);
+    });
+    
+});
